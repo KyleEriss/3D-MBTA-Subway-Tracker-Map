0f454b2b3e09c70c09558dc87477136bef1050fb
Kyle Eriss <keriss@mooveguru.com>
2023-10-03 00:27:49 -0400

    Refactoring to use connection pool for multiple users

--- server/src/Repositories/SubwayRepository.ts
+++ server/src/Repositories/SubwayRepository.ts
@@ -1,10 +1,27 @@
 const EventSource = require("eventsource");
 const { MBTA_API_KEY } = require("../config");
 
+// Define a type for the connection pool
+type EventSourcePool = {
+  [key: string]: EventSource;
+};
+
+// Create a connection pool
+const eventSourcePool: EventSourcePool = {};
+
 export async function getAllVehiclesEventSource(): Promise<EventSource> {
   let vehiclesEventSource = `https://api-v3.mbta.com/vehicles?api_key=${MBTA_API_KEY}`;
 
-  const serverSentEvent = await new EventSource(vehiclesEventSource.toString());
+  // const serverSentEvent = await new EventSource(vehiclesEventSource.toString());
 
-  return serverSentEvent;
+  // return serverSentEvent;
+
+  if (!eventSourcePool[vehiclesEventSource]) {
+    // If the connection doesn't exist, create a new one
+    eventSourcePool[vehiclesEventSource] = new EventSource(
+      vehiclesEventSource.toString()
+    );
+  }
+
+  return eventSourcePool[vehiclesEventSource];
 }
